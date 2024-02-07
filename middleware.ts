3e54afd566fa5737a47c03f3292faba346c08f27
Jerrick-Santos-DLSU <jerrickezekiel0122@gmail.com>
2024-02-07 14:55:59 +0800

    edited: -middleware.ts (catch case wherein there is no token in cookies

--- middleware.ts
+++ middleware.ts
@@ -7,18 +7,25 @@ export function middleware(req: NextRequest) {
     
     const { pathname }: { pathname: string } = req.nextUrl;
     const tokenAuth = req.cookies.get('token')?.value;
-    const user_type = (<any>jwtDecode(tokenAuth!)).user_type
+   
 
 
     const login_url = req.nextUrl.clone()
     login_url.pathname = '/login'
 
-    if(PERMISSIONS[user_type].includes(pathname)){
-        return NextResponse.next()
+    if(tokenAuth){
+        const user_type = (<any>jwtDecode(tokenAuth!)).user_type
+        if(PERMISSIONS[user_type].includes(pathname)){
+            return NextResponse.next()
+        }
+        else {
+            return NextResponse.redirect(login_url)
+        }
     }
-    else {
+    else{
         return NextResponse.redirect(login_url)
     }
+
 }
  
 export const config = {
