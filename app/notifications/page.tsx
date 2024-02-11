08ddf8c106933626ee709529ab46f8b9d0c02862
pattyarao-dev <kkenmapagmahal@gmail.com>
2024-02-11 22:53:13 +0800

    added: -notif page -sidebar menu todo: -fix sidebar animation -start frontend data fetching

---
+++ app/notifications/page.tsx
@@ -0,0 +1,12 @@
+import { NotificationsList } from '@/components/pages/notifications/notificationsList'
+import Header from '@/components/reusable/header'
+import React from 'react'
+
+export default function Notifications() {
+  return (
+    <main className="w-full min-h-screen flex flex-col">
+        <Header/>
+        <NotificationsList/>
+    </main>
+  )
+}
