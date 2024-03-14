f4df95ef3aab93c502a12abd81fb78b5f9035010
carlo-reyes <carlo_jose_reyes@dlsu.edu.ph>
2024-03-14 18:29:12 +0800

    edited: brandprofile, usermanagement, viewrewards suspense

--- app/viewrewards/page.tsx
+++ app/viewrewards/page.tsx
@@ -1,12 +1,14 @@
 import ViewRewardsComp from '@/components/pages/viewrewards/viewrewardsComponent'
 import MerchantHeader from '@/components/reusable/merchantheader'
-import React from 'react'
+import React, {Suspense} from 'react'
 
 export default function ViewRewards() {
     return (
-        <main className="w-full min-h-screen flex flex-col">
-            <MerchantHeader/>
-            <ViewRewardsComp/>
-        </main>
+        <Suspense fallback={<div>Loading...</div>}>
+            <main className="w-full min-h-screen flex flex-col">
+                <MerchantHeader/>
+                <ViewRewardsComp/>
+            </main>
+        </Suspense>
     )
 }
