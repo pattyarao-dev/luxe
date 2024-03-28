128a50eac70cb2a4012f60fb90bc2ce466218eb7
pattyarao-dev <kkenmapagmahal@gmail.com>
2024-03-28 15:36:02 +0800

    added: -claimed rewards history page -unclaimed rewards fetch for homepage

--- app/(client)/home/page.tsx
+++ app/(client)/home/page.tsx
@@ -13,63 +13,38 @@ import { ClientHomepage } from "@/components/pages/clienthome/ClientHomepage"
 //   const res = await fetch(`http://localhost:4000/rewards`);
 //   return res.json();
 // }
-async function getRewards() {
-    try {
-        const rewards = await Rewards.find().exec()
-        return rewards
-    } catch (error) {
-        console.error("Error fetching rewards", error)
-        return []
-    }
-}
+// async function getRewards() {
+//     try {
+//         const rewards = await Rewards.find().exec()
+//         return rewards
+//     } catch (error) {
+//         console.error("Error fetching rewards", error)
+//         return []
+//     }
+// }
 
-export default async function Home() {
-    // console.log(getTokenContent());
+export default function Home() {
     const user = getTokenContent()
     const { _id, user_type } = user
 
-    const rewards = await getRewards()
-
-    const saveReward = async (rewardId: ObjectId) => {
-        // Call your API endpoint to save the reward
-        // You may need to pass additional data to identify the user
-        try {
-            const res = await fetch(
-                `${process.env.NEXT_PUBLIC_API_URL}/reward/save?client=${user._id}&reward=${rewardId}`,
-                {
-                    method: "PATCH"
-                    // Additional options like headers and body can be added here
-                }
-            )
-            const data = await res.json()
-            console.log(data.message)
-        } catch (error) {
-            console.error("Error saving reward:", error)
-        }
-    }
+    // const saveReward = async (rewardId: ObjectId) => {
+    //     try {
+    //         const res = await fetch(
+    //             `${process.env.NEXT_PUBLIC_API_URL}/reward/save?client=${user._id}&reward=${rewardId}`,
+    //             {
+    //                 method: "PATCH"
+    //             }
+    //         )
+    //         const data = await res.json()
+    //         console.log(data.message)
+    //     } catch (error) {
+    //         console.error("Error saving reward:", error)
+    //     }
+    // }
 
     return (
         <main className="w-full min-h-screen primary-background">
-            <div className="w-full p-4 flex flex-col gap-3">
-                {/* <SearchBar /> */}
-                <h1 className="w-full text-2xl font-semibold">
-                    Browse Rewards
-                </h1>
-            </div>
-
-            <div className="w-full h-[75vh] p-4 flex flex-col gap-3 overflow-y-auto">
-                {rewards.map((reward) => (
-                    <div key={reward._id} className="w-full h-full">
-                        <RewardCard
-                            id={reward.id}
-                            reward_name={reward.reward_name}
-                            brand_name={reward.brand_name}
-                            userId={user._id}
-                        />
-                    </div>
-                ))}
-            </div>
-            {/* <ClientHomepage rewards={rewards} user={user} /> */}
+             <ClientHomepage user={user._id} />
         </main>
     )
 }
