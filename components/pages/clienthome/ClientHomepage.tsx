9e9b4006a4044555fe74169e5806cd27a9710962
pattyarao-dev <kkenmapagmahal@gmail.com>
2024-03-29 21:47:49 +0800

    fixed: layouts

--- components/pages/clienthome/ClientHomepage.tsx
+++ components/pages/clienthome/ClientHomepage.tsx
@@ -6,18 +6,25 @@ import { ObjectId } from "mongoose"
 import { RewardCard } from "./RewardCard"
 import SearchBar from "@/components/reusable/searchbar"
 import { IoSearch } from "react-icons/io5"
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io"
 
-export const ClientHomepage = ({user}: {user: ObjectId}) => {
+export const ClientHomepage = ({ user }: { user: ObjectId }) => {
     const [unclaimedRewards, setUnclaimedRewards] = useState<RewardTypes[]>([])
     const [searchTerm, setSearchTerm] = useState<string>("")
+    const [selectedBrand, setSelectedBrand] = useState<string>("")
+    const [openDropdown, setOpenDropdown] = useState<boolean>(false)
     const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSearchTerm(event.target.value)
     }
-    const filteredRewards = unclaimedRewards.filter((reward) =>
-        reward.reward_name.toLowerCase().includes(searchTerm.toLowerCase())
+    const filteredRewards = unclaimedRewards.filter(
+        (reward) =>
+            reward.reward_name
+                .toLowerCase()
+                .includes(searchTerm.toLowerCase()) ||
+            reward.brand_name.toLowerCase()
+        //     .includes(searchTerm.toLowerCase()) ||
+        // (selectedBrand && reward.brand_name === selectedBrand)
     )
-
-     
 
     useEffect(() => {
         const fetchUnclaimedRewards = async () => {
@@ -50,13 +57,15 @@ export const ClientHomepage = ({user}: {user: ObjectId}) => {
         }
     }
 
+    const uniqueBrands = Array.from(
+        new Set(filteredRewards.map((reward) => reward.brand_name))
+    )
+
     return (
         <div className="w-full min-h-screen">
             <div className="w-full p-4 flex flex-col gap-4">
-                <h1 className="w-full text-2xl font-bold">
-                    Browse Rewards
-                </h1>
-                <div className="w-full">
+                <h1 className="w-full text-2xl font-bold">Browse Rewards</h1>
+                <div className="w-full flex flex-col gap-4">
                     <div className="w-full flex items-center gap-2 border-2 p-3 rounded-lg border-dark-pink bg-white drop-shadow-lg">
                         <IoSearch className="text-dark-pink" />
                         <input
@@ -67,22 +76,58 @@ export const ClientHomepage = ({user}: {user: ObjectId}) => {
                             onChange={handleSearch}
                         />
                     </div>
+                    {/* <div className="w-full relative">
+                        <p
+                            onClick={() => setOpenDropdown(!openDropdown)}
+                            className={`w-1/2 p-2 bg-gray-200 flex justify-between items-center ${
+                                openDropdown ? "rounded-t-md" : "rounded-md"
+                            }`}
+                        >
+                            <span>Select brand</span>
+                            {openDropdown ? (
+                                <IoIosArrowUp />
+                            ) : (
+                                <IoIosArrowDown />
+                            )}
+                        </p>
+                        <div
+                            className={`w-1/2 absolute top-full left-0 bg-white rounded-b-md ${
+                                openDropdown ? "block" : "hidden"
+                            }`}
+                            style={{ zIndex: 10 }}
+                        >
+                            <div className="max-h-48 overflow-y-auto">
+                                {uniqueBrands.map((brand, index) => (
+                                    <p
+                                        key={index}
+                                        className="px-4 py-2 cursor-pointer hover:bg-gray-100 text-sm"
+                                        onClick={() => setSelectedBrand(brand)}
+                                    >
+                                        {brand}
+                                    </p>
+                                ))}
+                            </div>
+                        </div>
+                    </div> */}
                 </div>
-                
             </div>
 
-            <div className="w-full h-[75vh] p-4 flex flex-col gap-3 overflow-y-auto">
-                {filteredRewards.map((reward, index) => (
-                    <div key={index} className="w-full h-full">
-                        <RewardCard
-                            id={reward._id}
-                            reward_name={reward.reward_name}
-                            brand_name={reward.brand_name}
-                            userId={user}
-                        />
-                    </div>
-                ))}
-            </div>
+            {filteredRewards ? (
+                <div className="w-full h-[70vh] overflow-y-auto p-4 flex flex-col gap-3">
+                    {filteredRewards.map((reward, index) => (
+                        <div key={index} className="w-full h-full">
+                            <RewardCard
+                                id={reward._id}
+                                reward_name={reward.reward_name}
+                                brand_name={reward.brand_name}
+                                userId={user}
+                            />
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <p>Loading rewards</p>
+            )}
         </div>
     )
 }
