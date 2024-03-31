de74088cdb2b3b692404af2709e87e024386f435
pattyarao-dev <kkenmapagmahal@gmail.com>
2024-03-31 23:29:29 +0800

    added: -client logout

--- components/pages/profile/profileDetailsComponent.tsx
+++ components/pages/profile/profileDetailsComponent.tsx
@@ -1,115 +1,48 @@
 "use client"
 
 import { UserTypes } from "@/app/types/userTypes"
+import { useRouter } from "next/navigation"
 import React from "react"
 import { useState } from "react"
 import { FaUser } from "react-icons/fa"
 
-interface userDetailsType {
-    name: string
-    email: string
-    address: string
-    phone: number
-    birthdate: Date
-}
-
 export default function ProfileDetails({ user }: { user: UserTypes }) {
-    const [isEditClicked, setIsEditClicked] = useState<boolean>(false)
+    const { push } = useRouter()
+    const handleLogout = async () => {
+        try {
+            const response = await fetch("/api/logout", {
+                method: "POST",
+                credentials: "same-origin"
+            })
+            if (response.ok) {
+                // Logout successful, you might want to redirect or update UI accordingly
+                console.log("Logged out successfully")
+                push("/login")
+            } else {
+                console.error("Failed to logout:", response.statusText)
+            }
+        } catch (error) {
+            console.error("Error logging out:", error)
+        }
+    }
 
     return (
-        <main className="w-full min-h-screen">
-            <div className="w-full p-10 flex flex-col gap-4">
-                <div className="w-full flex items-center gap-6">
-                    <div className="bg-gray-main p-3 rounded-md">
-                        <FaUser className="text-5xl text-white" />
-                    </div>
-                    <div>
-                        <h1 className="text-2xl font-semibold text-dark-pink">{`${user.first_name} ${user.last_name}`}</h1>
-                        <h3 className="text-xs text-gray-main">{user.email}</h3>
-                    </div>
+        <>
+            <main className="w-2/3 h-72 flex flex-col items-center justify-center gap-6 bg-white rounded-md drop-shadow-md">
+                <div className="rounded-md">
+                    <FaUser className="text-5xl text-neutral-300" />
                 </div>
-                <hr className="w-full border border-dark-pink" />
-            </div>
-        </main>
-        // <>
-        //     {!isEditClicked ? (
-        //         <section className="mt-24 w-full flex flex-col justify-center items-center gap-10">
-        //             <div className="w-[80%] border border-dark-pink px-6 py-8 flex flex-col gap-2 rounded-xl drop-shadow-dark-pink bg-white">
-        //                 <div className="information">
-        //                     <h1 className="profile-title">Name</h1>
-        //                     <p className="profile-data">{`${user.first_name} ${user.last_name}`}</p>
-        //                 </div>
-        //                 <div className="information">
-        //                     <h1 className="profile-title">Email</h1>
-        //                     <p className="profile-data">{user.email}</p>
-        //                 </div>
-        //                 <div className="information">
-        //                     <h1 className="profile-title">Address</h1>
-        //                     <p className="profile-data">
-        //                         Metro Manila, Philippines
-        //                     </p>
-        //                 </div>
-        //                 <div className="information">
-        //                     <h1 className="profile-title">Phone</h1>
-        //                     <p className="profile-data">123456</p>
-        //                 </div>
-        //                 <div className="information">
-        //                     <h1 className="profile-title">Birthdate</h1>
-        //                     <p className="profile-data">01 / 01 / 91</p>
-        //                 </div>
-        //             </div>
-        //             <button
-        //                 className="gradient-button w-[80%] py-3"
-        //                 onClick={() => setIsEditClicked(!isEditClicked)}
-        //             >
-        //                 Edit Profile
-        //             </button>
-        //         </section>
-        //     ) : (
-        //         <section className="mt-24 w-full flex flex-col justify-center items-center gap-10">
-        //             <div className="w-[80%] border border-dark-pink px-6 py-8 flex flex-col gap-2 rounded-xl drop-shadow-dark-pink bg-white">
-        //                 <div className="information">
-        //                     <h1 className="profile-title">Name</h1>
-        //                     {/* <p className="profile-data">Juan Dela Cruz</p> */}
-        //                     <input
-        //                         type="text"
-        //                         className="profile-data focus:outline-none"
-        //                         placeholder="Edit your name"
-        //                     />
-        //                 </div>
-        //                 <div className="information">
-        //                     <h1 className="profile-title">Email</h1>
-        //                     <p className="profile-data">jdelacruz@gmail.com</p>
-        //                 </div>
-        //                 <div className="information">
-        //                     <h1 className="profile-title">Address</h1>
-        //                     <input
-        //                         type="text"
-        //                         className="profile-data focus:outline-none"
-        //                         placeholder="Edit your address"
-        //                     />
-        //                 </div>
-        //                 <div className="information">
-        //                     <h1 className="profile-title">Phone</h1>
-        //                     <input
-        //                         type="text"
-        //                         className="profile-data focus:outline-none"
-        //                         placeholder="Edit your number"
-        //                     />
-        //                 </div>
-        //                 <div className="information">
-        //                     <h1 className="profile-title">Birthdate</h1>
-        //                     <p className="profile-data">01 / 01 / 91</p>
-        //                 </div>
-        //             </div>
-        //             <button
-        //                 className="solid-button w-[80%] py-3"
-        //                 onClick={() => setIsEditClicked(!isEditClicked)}
-        //             >
-        //                 Save Changes
-        //             </button>
-        //         </section>
-        //     )}
-        // </>
+                <div className="w-full flex flex-col items-center">
+                    <h1 className="text-2xl font-semibold text-dark-pink">{`${user.first_name} ${user.last_name}`}</h1>
+                    <h3 className="text-xs text-gray-main">{user.email}</h3>
+                </div>
+            </main>
+            <button
+                className="text-dark-purple font-semibold uppercase"
+                onClick={handleLogout}
+            >
+                Logout
+            </button>
+        </>
     )
 }
